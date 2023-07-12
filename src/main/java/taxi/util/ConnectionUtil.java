8c044a4d328f812b292669304d52f78206f9a4d2
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2023-07-12 21:16:01 +0300

    Update ConnectionUtil.java

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -7,10 +7,10 @@ import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL =
-            "jdbc:mysql://localhost:3306/taxi";
+            "url";
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String JDBC_DRIVER = "driver";
 
     static {
         try {
